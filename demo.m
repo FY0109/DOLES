363a674277956951ff852433e401e392e7b274ca
FY0109 <135565179+FY0109@users.noreply.github.com>
2023-12-06 16:47:58 +0800

    Update demo.m

--- demo.m
+++ demo.m
@@ -1,25 +1,23 @@
 clc
 clear all
 warning off
-load("3Sources.mat")%load dataset
-
+%% Load the data.
+% Please uncomment the dataset that you want to test.
+load("3Sources.mat")
 disp('Dateset：3Sources')
 
 viewnum=length(X);
 
 opt.maxiter=50;
 
-
 data=X;
-k=length(unique(Y));
+k=length(unique(Y));% k is the number of cluster
 
 % Parameter setting
-d=[ 3*k ];
+d=[ 3*k ]; 
 l=[ 3*k];
-num_p=[3];
-lambda=[0.001 ];
-
-
+num_p=[3]; % num_p is the number of LES
+lambda=[0.001 ]; %this is default
 
 for v=1:viewnum
     data{v}=data{v};
@@ -34,8 +32,10 @@ for ll=1:length(l)
 for lm=1:length(lambda)
 
 tic;
+%Run MSC-DOLES
 [L,output]=DOLES(data,num_p(pp),d(dd),l(ll),k,lambda(lm),opt);
 toc
+%res save the scores of ACC, NMI, PUR and FScore.
 res = Clustering8Measure(Y,L);
 
 fprintf(' p:%d\t d:%d\t l:%d\t lambda:%12.5f\t ACC:%12.5f\t nmi:%12.5f\t Purity:%12.5f\t Fscore:%12.5f \t\n',[ num_p(pp) d(dd) l(ll) lambda(lm) res(1) res(2) res(3) res(4) ]);
